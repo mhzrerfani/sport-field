9fecd4968c232f1cab096e1affed6a591d59fc42
mhzrerfani <mhzrerfani@gmail.com>
2021-10-14 11:45:30 +0330

    configured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,20 @@ const config = {
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		fontFamily: {
+			vazir: ['vazir']
+		},
+		extend: {
+			colors: {
+				orange: '#DF6531',
+				lightBlue: '#E4F5FB',
+				blue: '#4D93E5',
+				gray1: '#ECECEC',
+				gray2: '#A6A4A4',
+				green: '#58CC30'
+			}
+		}
 	},
-
 	plugins: []
 };
 
